059393439bd78902f55bf4f70de2109eea261d4d
Huzaifa <huzaifatahir033@gmail.com>
2026-07-18 13:02:03 +0500

    Generic Calculator Of OOPS

--- CalculaterOOPS.cpp
+++ CalculaterOOPS.cpp
@@ -2,24 +2,24 @@
 #include <iomanip>
 using namespace std;
 
-template <class T>
+template <class H>
 class Calculator {
 private:
-    T v1, v2;
+    H v1, v2;
 
 public:
-    Calculator(T n1, T n2) {
+    Calculator(H n1, H n2) {
         v1 = n1;
         v2 = n2;
     }
 
-    T add() { return v1 + v2; }
+    H add() { return v1 + v2; }
     
-    T subtract() { return v1 - v2; }
+    H subtract() { return v1 - v2; }
     
-    T multiply() { return v1 * v2; }
+    H multiply() { return v1 * v2; }
 
-    T divide() {
+    H divide() {
         if (v2 == 0) {
             cout << "Error: div by zero\n";
             return 0; 
@@ -27,54 +27,53 @@ public:
         return v1 / v2;
     }
 
-    void displayResult() {
-        cout << fixed << setprecision(2);
-        cout << "\n--- Results ---\n";
-        cout << "Value 1: " << v1 << "\nValue 2: " << v2 << "\n";
-        cout << "Sum: " << add() << "\n";
-        cout << "Diff: " << subtract() << "\n";
-        cout << "Product: " << multiply() << "\n";
+    void displayOutput() {
+        cout << fixed << setprecision(4);
+        cout << " Outputs" <<endl;
+        cout << "Value 1: " << v1 << "\nValue 2: " << v2 <<endl;
+        cout << "Sum: " << add() <<endl;
+        cout << "Diff: " << subtract() <<endl;
+        cout << "Product: " << multiply() <<endl;
         
         cout << "Div: ";
         if (v2 == 0)
-            cout << "undefined\n";
+            cout << "undefined"<<endl;
         else
-            cout << divide() << "\n";
-        cout << "---------------\n";
+            cout << divide() << endl;
     }
 };
 
 int main() {
-    int opt;
+    int selection;
 
-    cout << "Select Data Type:\n";
-    cout << "1. Integer (int)\n2. Decimal (float)\n3. Large Decimal (double)\n";
+    cout << "Select Data Type:"<<endl;;
+    cout << "1. Integer (int)\n2. Decimal (float)\n3. Large Decimal (double)"<<endl;
     cout << "Enter (1-3): ";
-    cin >> opt;
+    cin >> selection;
 
-    if (opt == 1) {
-        int x, y;
-        cout << "Enter 1st int: "; cin >> x;
-        cout << "Enter 2nd int: "; cin >> y;
-        Calculator<int> calc(x, y);
-        calc.displayResult();
+    if (selection == 1) {
+        int h, m;
+        cout << "Enter 1st int: "; cin >> h;
+        cout << "Enter 2nd int: "; cin >> m;
+        Calculator<int> calc(h, m);
+        calc.displayOutput();
     }
-    else if (opt == 2) {
-        float x, y;
-        cout << "Enter 1st float: "; cin >> x;
-        cout << "Enter 2nd float: "; cin >> y;
-        Calculator<float> calc(x, y);
-        calc.displayResult();
+    else if (selection == 2) {
+        float h, m;
+        cout << "Enter 1st float: "; cin >> h;
+        cout << "Enter 2nd float: "; cin >> m;
+        Calculator<float> calc(h, m);
+        calc.displayOutput();
     }
-    else if (opt == 3) {
-        double x, y;
-        cout << "Enter 1st double: "; cin >> x;
-        cout << "Enter 2nd double: "; cin >> y;
-        Calculator<double> calc(x, y);
-        calc.displayResult();
+    else if (selection == 3) {
+        double h, m;
+        cout << "Enter 1st double: "; cin >> h;
+        cout << "Enter 2nd double: "; cin >> m;
+        Calculator<double> calc(h, m);
+        calc.displayOutput();
     }
     else {
-        cout << "wrong input\n";
+        cout << "wrong input"<<endl;
     }
 
     return 0;
